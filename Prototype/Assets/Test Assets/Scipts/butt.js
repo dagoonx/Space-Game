408dff1c3aa7ba2edbc89b3c3019636d0455b1b7
Dagoonx <sparky_kool_k@hotmail.com>
2014-06-08 16:21:31 -0400

    Got the Jist of the protype working, tagged some ojects to pick up and added both the Raycast Reachscript(now known as Interaction) and the Butt script for buttons

---
+++ Prototype/Assets/Test Assets/Scipts/butt.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+static var amOn = false;
+var thePrefab : GameObject;
+var sP : GameObject;
+
+
+
+function Start () {
+	sP = GameObject.Find("swnPnt");
+	
+	
+
+}
+
+function Update () {
+	
+	
+	
+	if (amOn == true){
+	
+		renderer.material.color = Color.blue;
+		
+		Instantiate(thePrefab, sP.transform.position, sP.transform.rotation);
+		
+		butt.amOn = false;
+	}
+
+}
