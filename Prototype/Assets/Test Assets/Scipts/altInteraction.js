3d5b7170882c8194eef296c232ab3e9384283290
Dagoonx <sparky_kool_k@hotmail.com>
2014-06-17 15:46:10 -0400

    trying to make a new selection system based off old one
    Signed-off-by: Dagoonx <sparky_kool_k@hotmail.com>

---
+++ Prototype/Assets/Test Assets/Scipts/altInteraction.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+var hit : RaycastHit;
+
+function Start () {
+	
+}
+
+function Update () {
+
+	
+	if (Input.GetKeyDown(KeyCode.E)){
+		shootRay();
+		if (hit.transform.tag == "physTest") {
+			pickUp();
+		}
+	}
+
+}
+
+function shootRay() : boolean {
+	var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+	return Physics.Raycast(ray, hit, 10);
+}
+
+function pickUp() {
+	var snapPar = GameObject("emptySnapPar");
+
+}
+
+function use() {
+
+}
